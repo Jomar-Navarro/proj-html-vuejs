2fb492814aa70d9fd6aa0a7ef20949a2b3d497fd
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-04-08 20:51:59 +0200

    [add] jumbo done

--- src/components/jumbo.vue
+++ src/components/jumbo.vue
@@ -5,35 +5,53 @@
 </script>
 
   <template>
-    <div class="container text-center my-5">
-      <div class="row align-items-center">
-        <div class="col-4 text-start">
-          <h1>Distant learning for further expansion</h1>
-          <p>Learning is a life-long journey that in fact we never find the terminate stop. Stop searching, enjoy the process</p>
-          <button><i class="fa-solid fa-download"></i> <span>Download free guide</span></button>
-          <p>Have questions? <a href="#">Get Free Sample &rightarrow;</a></p>
-        </div>
-        <div id="other-bg-jumbo" class="col-8 bg-jumbo">
-          <img src="/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="#">
+    <div class="circle">
+      <div class="container text-center my-5">
+        <div class="row align-items-center">
+          <div class="col-5 text-start">
+            <h1>Distant learning for further expansion</h1>
+            <p>Learning is a life-long journey that in fact we never find the terminate stop. Stop searching, enjoy the process</p>
+            <button><i class="fa-solid fa-download"></i> <span><strong>Download free guide</strong></span></button>
+            <p class="link">Have questions? <a href="#">Get Free Sample <i class="fa-solid fa-arrow-right-long"></i></a></p>
+          </div>
+          <div class="col-7 bg-jumbo">
+            <img src="/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="#">
+          </div>
         </div>
       </div>
     </div>
+
   </template>
 
 <style lang="scss" scoped>
 @use '/src/assets/scss/main.scss' as *;
 
+.circle{
+  display: block;
+  background: url('/img/maxcoach-shape-02.png') right;
+  background-repeat: no-repeat;
+  background-position: 85% 17%;
+}
+
 .container{
   height: 100vh;
-  
+  h1{
+    color: $title-color;
+    font-size: 3rem;
+    font-weight: 700;
+  }
+  p{
+    color: $jumbotext;
+    font-size: 1.1rem;
+    font-weight: 500;
+  }
   .bg-jumbo{
     background:
-    url('/img/maxcoach-shape-02.png') right,
     url('/img/maxcoach-shape-07.png') right bottom,
     url('/img/maxcoach-shape-08.png'),
     url('/img/maxcoach-shape-08.png');
     background-repeat: no-repeat, no-repeat,;
-    background-position: right, 68% 99%, 10% 40%, 10% 73%;
+    background-position: 68% 100%, 5% 40%, 5% 73%;
   }
   img{
     width: max-content;
@@ -42,7 +60,24 @@
   }
 }
 
-#dot{
-  position: absolute;
+.container{
+  button{
+    background-color: $text-green;
+    color: white;
+    padding: 10px 40px;
+    border: none;
+    border-radius: 5px;
+    margin: 20px 0px;
+  }
+
+  .link{
+  color: $jumbotext;
+  font-size: 0.9rem;
+  font-weight: 500;
+    a{
+      text-decoration-color: lightgrey;
+      color: $text-green;
+    }
+  }
 }
 </style>
