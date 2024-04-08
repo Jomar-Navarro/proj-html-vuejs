16714bdbfaaa1cf77877b3e2e6450468c1ef27f2
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-04-08 16:59:20 +0200

    [add] beginning of the jumbo

--- src/components/jumbo.vue
+++ src/components/jumbo.vue
@@ -6,10 +6,28 @@
 
   <template>
     <div class="container text-center">
-      Jumbo
+      <div class="row d-flex justify-content-center">
+        <div class="col-4 text-start">
+          <h1>Distant learning for further expansion</h1>
+          <p>Learning is a life-long journey that in fact we never find the terminate stop. Stop searching, enjoy the process</p>
+          <button><i class="fa-solid fa-download"></i> <span>Download free guide</span></button>
+          <p>Have questions? <a href="#">Get Free Sample &rightarrow;</a></p>
+        </div>
+        <div class="col-8">
+          <img src="/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="">
+        </div>
+      </div>
     </div>
   </template>
 
 <style lang="scss" scoped>
+@use '/src/assets/scss/main.scss' as *;
 
+.container{
+  height: 100vh;
+  img{
+    width: max-content;
+    border-radius: 50%;
+  }
+}
 </style>
