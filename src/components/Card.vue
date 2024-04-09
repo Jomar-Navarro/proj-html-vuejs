c585481e337721b198c831536b3b6b8b8583c464
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-04-09 10:07:16 +0200

    [add] dinamic card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,14 +18,14 @@ import { store } from '../data/store';
     <h1>Featured Online Courses</h1>
 
     <div class="d-flex flex-wrap justify-content-center">
-      <div v-for="item in 6" :key="item" class="card mt-5 border-0" style="max-width: 540px;">
+      <div v-for="(item,index) in store.cardContent" :key="index" class="card mt-5 border-0" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4 square d-flex">
-            <img src="/img/course-02-480x298.jpg" class="img-fluid rounded-circle" alt="#">
+            <img :src="item.cardImage" class="img-fluid rounded-circle" alt="#">
           </div>
           <div class="col-md-8">
             <div class="card-body text-start ps-4">
-              <h4 class="card-title">$40.00</h4>
+              <h4 class="card-title">{{ item.price }}</h4>
               <h4 class="card-text">Learning to Write as a Professional Author</h4>
               <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
--- src/data/store.js
+++ src/data/store.js
@@ -4,43 +4,43 @@ export const store = reactive({
   mainTitle: 'MaxCoach',
   cardContent: [
           {
-            cardImage: '',
-            price: '',
+            cardImage: '/img/course-02-480x298.jpg',
+            price: '$40.00',
             program: '',
             class: '',
           },
 
           {
-            cardImage: '',
-            price: '',
+            cardImage: '/img/stock-full-hd-03-480x298.jpg',
+            price: 'Free',
             program: '',
             class: '',
           },
 
           {
-            cardImage: '',
-            price: '',
+            cardImage: '/img/stock-full-hd-04-480x298.jpg',
+            price: '$19.00',
             program: '',
             class: '',
           },
 
           {
-            cardImage: '',
-            price: '',
+            cardImage: '/img/stock-full-hd-06-480x298.jpg',
+            price: '$26.00',
             program: '',
             class: '',
           },
 
           {
-            cardImage: '',
-            price: '',
+            cardImage: '/img/course-featured-image-01-480x298.jpg',
+            price: '$39.00',
             program: '',
             class: '',
           },
 
           {
-            cardImage: '',
-            price: '',
+            cardImage: '/img/stock-full-hd-05-480x298.jpg',
+            price: '$59.00',
             program: '',
             class: '',
           },
