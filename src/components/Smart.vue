235f74945dce9c7f5dec13e9ece6684d5c525c44
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-04-09 12:12:40 +0200

    [add] smart done

--- src/components/Smart.vue
+++ src/components/Smart.vue
@@ -1,37 +1,50 @@
 <script>
   export default {
-    
+    data(){
+      return{
+        content: [
+          {
+            cardImage: '/img/home-2-image-box-01.png',
+            title: 'Customize Preferences',
+            text: 'Learners are given the right to arrange and customize their study schedule and timetable based on preferences.',
+            link: 'Start here',
+          },
+
+          {
+            cardImage: '/img/about-us-02-image-box-02.png',
+            title: 'Cloud Storage',
+            text: 'You can store online lessons via online cloud or download to your device and revise lessons on the way',
+            link: 'Get Free Quote',
+          },
+
+          {
+            cardImage: '/img/home-2-image-box-03.png',
+            title: 'Multimedia Channels',
+            text: 'We provide study material with varius formats: video, audio, slides, doc, prints, books, and applications.',
+            link: 'Start Now',
+          }
+        ]
+      }
+    }
   }
 </script>
 
   <template>
-    <div class="container text-center d-flex justify-content-center">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+    <div class="container text-center">
+      <h6 class="sub-title text-uppercase">maxcoach available courses</h6>
+      <h2 class="title">Access <span>Smart Tutoring</span> Program For Benefits</h2>
+
+      <div class="d-flex justify-content-center text-start mt-5">
+        <div v-for="item in content" :key="item" class="card" style="width: 18rem;">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+              <img :src="item.cardImage" class="card-img-top" alt="...">
+              <h4 class="card-title my-3">{{ item.title }}</h4>
+              <p class="card-text ">{{ item.text }}</p>
+              <p><a href="#">{{ item.link }} <i class="fa-solid fa-arrow-right-long"></i></a></p>
           </div>
+        </div>
       </div>
 
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div>
     </div>
   </template>
 
@@ -40,6 +53,46 @@
 
 .container{
   margin-top: 200px;
+}
+
+.sub-title{
+  color: $jumbotext;
+  font-weight: 500;
+}
+
+.title{
+  color: $title-color;
+  font-weight: 700;
+  span{
+    color: $text-green;
+    font-weight: 400;
+  }
+}
+
+.card{
+  border: none;
+  margin: 2rem 5rem;
+  .card-img-top{
+    width: fit-content;
+  }
+}
+
+.card-body{
+  width: 130%;
+  p{
+    color: $text-footer;
+    font-size: 0.99rem;
+    font-weight: 400;
+  }
+
+  p:last-child{
+    a{
+      color: $jumbotext;
+      font-weight: 800;
+      text-decoration: none;
+      font-size: 0.8rem;
+    }
+  }
 }
 
 </style>
